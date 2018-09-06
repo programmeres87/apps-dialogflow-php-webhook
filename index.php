34b142c7c777ec082d61ecde8a2a4ef05772faa9
programmeres87 <43022296+programmeres87@users.noreply.github.com>
2018-09-06 11:48:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if($method == 'POST'){
 	}
 
 	$response = new \stdClass();
-	$response->fulfillmentText = $fulfillmentText;
+	$response->fulfillmentMessages->text = $fulfillmentText;
 	$response->displayText = $fulfillmentText;
 	$response->source = "webhook";
 	echo json_encode($response);
