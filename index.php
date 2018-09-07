0f7b06c90be8b35bb2f2c9feb284fe4d45328fed
programmeres87 <43022296+programmeres87@users.noreply.github.com>
2018-09-07 10:36:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,38 @@
 <?php 
-
 $method = $_SERVER['REQUEST_METHOD'];
 
-// Process only when method is POST
-if($method == 'POST'){
+if ($method == 'POST') {
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	
-	//var_dump($json);
-	$text = $json->queryResult->parameters->text;
+	$paramGeoCity = $json->queryResult->parameters->geo_city;
 	
-	//echo $text;
+	echo $paramGeoCity;
 
+	//echo $text;
 	switch ($text) {
 		case 'hi':
 			$fulfillmentText = "Hi, Nice to meet you";
 			break;
-
 		case 'bye':
 			$fulfillmentText = "Bye, good night";
 			break;
-
 		case 'anything':
 			$fulfillmentText = "Yes, you can type anything here.";
 			break;
-		
+		case 'dialog':
+			$fulfillmentText = "Yes, you can type dialog here.";
+			break;		
 		default:
 			$fulfillmentText = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
-
 	$response = new \stdClass();
 	$response->fulfillmentText = $fulfillmentText;
 	//$response->displayText = $fulfillmentText;
 	//$response->source = "webhook";
 	echo json_encode($response);
-}
-else
-{
+} else {
 	echo "Method not allowed by els";
 }
-
 ?>
